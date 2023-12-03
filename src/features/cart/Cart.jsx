de09bec97ebc1c275006112675c2479dc24be034
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-03 17:06:06 +0200

    Styling the Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-key */
 import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -30,15 +32,24 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className='py-3 px-4'>
       <LinkButton to="/menu" >&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-lg font-semibold'>Your cart, %NAME%</h2>
 
-      <div>
+      <ul className='divide-y-2 divide-stone-300 border-b-2 border-stone-300 mt-3'>
+        {cart.map((item) => 
+         ( 
+         <CartItem item={item} key={item.id}/>
+         )
+)}
+      </ul>
+
+      <div className='mt-6 space-x-4'>
         <Button to="/order/new" type="primary">Order pizzas</Button>
         {/* <Link ></Link> */}
-        <button>Clear cart</button>
+        
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,8 @@ export default function Button({children, disabled, to, type}) {
 
 const styles = {
   primary: base + "px-4 py-3 md:px-6 md:py-4",
-  small: base + "py-2 md:px-5 md:py-2.5 text-xs"
+  small: base + "py-2 md:px-5 md:py-2.5 text-xs",
+  secondary: " border-2 border-stone-400 uppercase font-semibold rounded-xl text-stone-800 inline-block tracking-wide px-2 py-2 hover:bg-stone-300 transition-all duration-300 focus:ring focus:ring-stone-800 focus:ring-offset"
 }
 
 
