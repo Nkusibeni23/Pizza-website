6b50b1a54f34a99ac0bf1784231424198653c19b
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-03 14:01:42 +0200

    Styling form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ export default function SearchOrder() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-2xl px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-600 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 "
       />
     </form>
   );
