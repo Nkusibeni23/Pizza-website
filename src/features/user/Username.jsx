8fb7257923a6c56e271b2b8d358c1dcf929c60ce
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-03 19:38:00 +0200

    user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,7 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="text-sm font-semibold hidden md:block">Benny</div>;
+const username = useSelector(state=>state.user.username)
+if(!username) return null;
+  return <div className="text-sm font-semibold hidden md:block">{username}</div>;
 }
