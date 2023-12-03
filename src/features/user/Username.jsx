f1531b58be7701fa717018fe2e6200a23c2cf710
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-03 13:05:51 +0200

    using FlexBox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="text-sm font-semibold sm:hidden">Benny</div>;
+  return <div className="text-sm font-semibold hidden md:block">Benny</div>;
 }
