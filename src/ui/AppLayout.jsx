c1afb98025d7e7e32fe387ebe071f47258ea49ce
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-03 15:21:32 +0200

    Absolute Position, z-index, justify Center Loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,6 +9,7 @@ export default function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
+      {/* {true && <Loader />} */}
       <Header />
       <div className="overflow-scroll my-10">
       <main className="max-w-3xl mx-auto">
